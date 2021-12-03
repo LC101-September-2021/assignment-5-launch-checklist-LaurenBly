7fe9ca89321d36de2f20bda3d0b35de1176406b6
LaurenBly <lauren.d.bly@gmail.com>
2021-12-02 18:14:45 -0600

    first commit

--- script.js
+++ script.js
@@ -13,4 +13,19 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+    const formSubmitButton = document.getElementById("pilotName");
+    let copilotName = document.getElementsByName("copilotName").item(0);
+    let fuelLevel = document.querySelector("input[name=fuelLevel]"); // querySelector gives you more "fine control" on what you're selecting
+    let cargoMass = document.querySelector("input[name=cargoMass]");
+
+    if (!pilotName.value.length) {
+        alert("You forgot to enter the pilot's name");
+    } if (!copilotName.value.length) {
+        alert("Don't forget about the co-pilot!");
+    } if (!fuelLevel.value.length) {
+        alert("You're gonna need some fuel to fly!");
+    } if(!cargoMass.value.length) {
+        alert("What's the cargo mass?");
+    }
+
 });
